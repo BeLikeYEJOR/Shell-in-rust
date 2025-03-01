a992957d1bb1e9ceca759d34ff35eecb2b7ab32d
BeLikeYEJOR <yejor599@gmail.com>
2025-02-28 18:33:19 -0800

    mad the code cleaner

---
+++ src/command_functions.rs
@@ -0,0 +1,87 @@
+use std::env;
+use std::fs;
+use std::collections::HashMap;
+// mod command_list;
+// use command_list::command_setup;
+// use command_list::get_prompt;
+
+pub fn ls() {
+    let current_dir = match env::current_dir() {
+        Ok(dir) => dir,
+        Err(e) => {
+            println!("Error retrieving current directory: {}", e);
+            return;
+        }
+    };
+    match fs::read_dir(&current_dir) {
+        Ok(entries) => {
+            for entry_result in entries {
+                match entry_result {
+                    Ok(entry) => {
+                        let file_name = entry.file_name().to_string_lossy().to_string();
+                        match entry.metadata() {
+                            Ok(metadata) => {
+                                if metadata.is_dir() {
+                                    println!("{}/", file_name);
+                                } else {
+                                    println!("{}", file_name);
+                                }
+                            },
+                            Err(e) => println!("Error reading metadata: {}", e),
+                        }
+                    },
+                    Err(e) => println!("Error reading directory entry: {}", e),
+                }
+            }
+        },
+        Err(e) => println!("Error reading directory: {}", e),
+    }
+}
+
+pub fn cd(tail: String) {
+    match tail.as_str() {
+        s if s.is_empty() => {
+            match env::current_dir() {
+                Ok(current_path) => println!("{}", current_path.display()),
+                Err(e) => println!("Error retrieving current directory: {}", e),
+            }
+        },                    
+        s if s == ".." => {
+            match env::current_dir() {
+                Ok(current_path) => {
+                    if let Some(parent) = current_path.parent() {
+                        if let Err(e) = env::set_current_dir(parent) {
+                            println!("cd: failed to change directory: {}", e);
+                        }
+                    } else {
+                        println!("cd: no parent directory");
+                    }
+                },
+                Err(e) => println!("cd: error getting current directory: {}", e),
+            }
+        },
+        _ => {
+            if let Err(e) = env::set_current_dir(&tail) {
+                println!("cd: {}: {}", &tail, e);
+            }
+        },
+    }
+}
+
+pub fn help(tail: String, list_of_commands:&HashMap<&str, &str>) {
+    match tail.as_str() {
+        s if s.is_empty() => {
+            for (cmd, desc) in list_of_commands {
+                println!("{} - {}", cmd, desc);
+            }
+        },
+        s if list_of_commands.get(s).is_some() => {
+            if let Some(desc) = list_of_commands.get(s) {
+                println!("{} - {}", tail, desc);
+            }
+        },
+        _ => {
+            println!("{}: is not a valid command", tail);
+        },
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::env;
+// use std::env;
 use std::fs;
 use std::io::{self, Write};
 mod command_list;
+mod command_functions;
+use command_functions::ls;
+use command_functions::cd;
+use command_functions::help;
 use command_list::command_setup;
 use command_list::get_prompt;
 
@@ -13,7 +17,7 @@ fn main() {
     
     loop {
         // Use get_prompt() to display a shorter prompt.
-        print!("~ {} $ ", get_prompt());
+        print!("{} $ ", get_prompt());
         io::stdout().flush().unwrap();
 
         let mut input = String::new();
@@ -29,25 +33,11 @@ fn main() {
             Some("exit") => std::process::exit(0),
             Some("echo") => println!("{}", tail),
             Some("type") => match tail.as_str() {
-                "echo" | "exit" | "type" | "help" | "cd" | "pwd" => println!("{tail} is a shell builtin"),
+                "echo" | "exit" | "type" | "help" | "cd"  => println!("{tail} is a shell builtin"),
                 _ => println!("{tail}: not found"),
             },
             Some("help") => {
-                match tail.as_str() {
-                    s if s.is_empty() => {
-                        for (cmd, desc) in &list_of_commands {
-                            println!("{} - {}", cmd, desc);
-                        }
-                    },
-                    s if list_of_commands.get(s).is_some() => {
-                        if let Some(desc) = list_of_commands.get(s) {
-                            println!("{} - {}", tail, desc);
-                        }
-                    },
-                    _ => {
-                        println!("{}: is not a valid command", tail);
-                    },
-                }
+                help(tail, &list_of_commands);
             },
             Some("cls") | Some("clear") => println!("\x1B[2J\x1B[1;1H"),
             Some("history") => {
@@ -55,35 +45,7 @@ fn main() {
                     println!("{}: {}", i + 1, cmd);
                 }
             },
-            Some("cd") => {
-                match tail.as_str() {
-                    s if s.is_empty() => {
-                        match env::current_dir() {
-                            Ok(current_path) => println!("{}", current_path.display()),
-                            Err(e) => println!("Error retrieving current directory: {}", e),
-                        }
-                    },                    
-                    s if s == ".." => {
-                        match env::current_dir() {
-                            Ok(current_path) => {
-                                if let Some(parent) = current_path.parent() {
-                                    if let Err(e) = env::set_current_dir(parent) {
-                                        println!("cd: failed to change directory: {}", e);
-                                    }
-                                } else {
-                                    println!("cd: no parent directory");
-                                }
-                            },
-                            Err(e) => println!("cd: error getting current directory: {}", e),
-                        }
-                    },
-                    _ => {
-                        if let Err(e) = env::set_current_dir(&tail) {
-                            println!("cd: {}: {}", &tail, e);
-                        }
-                    },
-                }
-            },
+            Some("cd") => cd(tail),
             Some("mkdir") => {
                 if let Err(e) = fs::create_dir_all(tail) {
                     println!("{}", e);
@@ -104,38 +66,7 @@ fn main() {
                     println!("{}", e);
                 }
             },
-            Some("ls") => {
-                let current_dir = match env::current_dir() {
-                    Ok(dir) => dir,
-                    Err(e) => {
-                        println!("Error retrieving current directory: {}", e);
-                        continue;
-                    }
-                };
-                match fs::read_dir(&current_dir) {
-                    Ok(entries) => {
-                        for entry_result in entries {
-                            match entry_result {
-                                Ok(entry) => {
-                                    let file_name = entry.file_name().to_string_lossy().to_string();
-                                    match entry.metadata() {
-                                        Ok(metadata) => {
-                                            if metadata.is_dir() {
-                                                println!("{}/", file_name);
-                                            } else {
-                                                println!("{}", file_name);
-                                            }
-                                        },
-                                        Err(e) => println!("Error reading metadata: {}", e),
-                                    }
-                                },
-                                Err(e) => println!("Error reading directory entry: {}", e),
-                            }
-                        }
-                    },
-                    Err(e) => println!("Error reading directory: {}", e),
-                }
-            },            
+            Some("ls") => ls(),   
             _ => println!("{}: command not found", input.trim()),
         }
     }
